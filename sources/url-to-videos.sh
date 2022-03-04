cd7973e1dad643612f430c96085b0ee0ddb640a1
Gaurav <allmanpride@gmail.com>
2022-03-04 14:40:20 -0500

    Fix: new gogo algorithm

--- sources/url-to-videos.sh
+++ sources/url-to-videos.sh
@@ -3,18 +3,20 @@
 # made from a function in:
 # https://github.com/pystardust/ani-cli/blob/master/ani-cli
 
-link=$(curl -s $1 | sed -n -E 's/^[[:space:]]*<a href="#" rel="100" data-video="([^"]*)".*/\1/p' | sed 's/^/https:/g')
+link=$(curl -s $1 | sed -n -E 's/.*class="active" rel="1" data-video="([^"]*)".*/\1/p' | sed 's/^/https:/g')
 
-ajax_url='https://gogoplay.io/encrypt-ajax.php'
+secret_key='3235373136353338353232393338333936313634363632323738383333323838'
+iv='31323835363732393835323338333933'
+ajax_url="https://gogoplay4.com/encrypt-ajax.php"
+crypto_data=$(curl -s "$link" | sed -nE 's/.*data-value="([^"]*)".*/\1/p')
+id=$(printf '%s' "$crypto_data" | base64 -d | openssl enc -d -aes256 -K "$secret_key" -iv "$iv" | cut -d '&' -f1)
 
-#get the id from the url
-video_id=$(printf "%s" "$link" | cut -d\? -f2 | cut -d\& -f1 | sed 's/id=//g')
+#encrypt and create the final ajax
+ajax=$(printf "%s\010\016\003\010\t\003\004\t" "$id" | openssl enc -aes256 -K "$secret_key" -iv "$iv" -a)
 
-#construct ajax parameters
-secret_key='3235373436353338353932393338333936373634363632383739383333323838'
-iv='34323036393133333738303038313335'
+#send request and get the data(most lamest way)
+data=$(curl -s -H "X-Requested-With:XMLHttpRequest" "$ajax_url" -d "id=$ajax" | sed -e 's/{"data":"//' -e 's/"}/\n/' -e 's/\\//g')
 
-ajax=$(printf "$video_id" | openssl enc -aes256  -K "$secret_key" -iv "$iv" -a)
-
-#send the request to the ajax url
-curl -s -H 'x-requested-with:XMLHttpRequest' "$ajax_url" -d "id=$ajax" -d "time=69420691337800813569" | jq -r '.source[].file'
+#decrypt the data to get final links
+printf '%s' "$data" | base64 -d | openssl enc -d -aes256 -K "$secret_key" -iv "$iv" | sed -e 's/\].*/\]/' -e 's/\\//g' |
+    grep -Eo 'https:\/\/[-a-zA-Z0-9@:%._\+~#=][a-zA-Z0-9][-a-zA-Z0-9@:%_\+.~#?&\/\/=]*'
